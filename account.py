4969c13191ce399198cd92ab58f1b3d84117b983
jessego20 <jessego@umich.edu>
2023-01-04 19:57:54 -0500

    .

---
+++ account.py
@@ -0,0 +1,21 @@
+import config
+import requests
+import json
+import locale
+
+def get_account_info(keys):
+    locale.setlocale(locale.LC_ALL, '')
+
+    r = requests.get(f'{config.BASE_URL}/v2/account', headers=keys)
+    account = json.loads(r.content)
+
+    account_info = {
+        'Portfolio Value': locale.currency(float(account['portfolio_value']), grouping=True),
+        'Cash': locale.currency(float(account['cash']), grouping=True),
+        'Position Market Value': locale.currency(float(account['position_market_value']), grouping=True),
+        'Long Market Value': locale.currency(float(account['long_market_value']), grouping=True),
+        'Short Market Value': locale.currency(float(account['short_market_value']), grouping=True),
+    }
+    print(json.dumps(account_info, indent=4))
+
+    # get_account_info(config.KEYS)
---
+++ first.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import datetime
+
+import config
+from account import get_account_info
+
+# Market Data imports
+from alpaca.data import CryptoHistoricalDataClient, StockHistoricalDataClient
+from alpaca.data import CryptoDataStream, StockDataStream
+from alpaca.data.requests import CryptoBarsRequest, StockBarsRequest
+
+get_account_info(config.KEYS)
