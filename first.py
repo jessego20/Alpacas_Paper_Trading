86e2528f1448f28724a3d861dad119d81cf9a74a
jessego20 <jessego@umich.edu>
2023-01-06 17:45:00 -0500

    .

--- first.py
+++ first.py
@@ -12,6 +12,9 @@ from alpaca.data.requests import CryptoBarsRequest, StockBarsRequest
 from alpaca.data.timeframe import TimeFrame
 # Technical Analysis import
 import btalib
+# Company financials import
+import simfin as sf
+from simfin.names import *
 
 '''
 Parameters: 
@@ -39,19 +42,41 @@ def main():
     account.get_account_info(config.KEYS)
 
     stock_client = StockHistoricalDataClient(config.API_KEY,  config.SECRET_KEY)
-
-    with open('data/qqq.csv') as f:
+    
+    stock_index = 'qqq'
+    with open(f'data/{stock_index}.csv') as f:
         qqq_holdings = f.readlines()
     
     symbols = [holding.split(',')[2].strip() for holding in qqq_holdings[1:]]
     timeframe = TimeFrame.Day
     start = datetime.datetime(2022,1,1)
-    end = datetime.datetime(2022,12,31)
-    df = get_stock_historical_data(stock_client, symbols, timeframe, start, end)
+    for symbol in symbols:
+        df = get_stock_historical_data(stock_client, symbol, timeframe, start)
+
+        # technical indicators
+        sma = btalib.sma(df, period = 30)
+        rsi = btalib.rsi(df)
+        macd = btalib.macd(df)
+        df['30sma'] = sma.df
+        df['rsi'] = rsi.df
+        df['macd'] = macd.df['macd']
+        df['signal'] = macd.df['signal']
+        df['histogram'] = macd.df['histogram']
+
+        filename = f'data/daily/{symbol}.txt'
+        with open(filename, 'w') as f:
+            f.write(df.loc[symbol].to_csv())
+        
+    
+    df = pd.read_csv('data/daily/AAPL.txt', parse_dates=True, index_col='timestamp')
     print(df)
 
-    sma = btalib.sma(df, period = 30)
-    print(sma.df)
+    # Getting financial data
+    # sf.set_data_dir('data/simfin/')
+    # sf.set_api_key(api_key=config.SIMFIN_KEY)
+    # df_income = sf.load_income(variant='annual', market='us')
+    # print(df_income.loc['AAPL'])
+        
 
 if __name__ == '__main__':
     main()
