69ded2bbe6e2e71b3f66ac9b8b02152279877aaf
jessego20 <jessego@umich.edu>
2023-01-09 14:51:19 -0500

    .

--- first.py
+++ first.py
@@ -26,9 +26,9 @@ Parameters:
 Returns:
     pandas dataframe with stock data
 '''
-def get_stock_historical_data(stock_client, symbols, timeframe, start=None, end=None):
+def get_stock_historical_data(stock_client, symbol, timeframe, start=None, end=None):
     request_params = StockBarsRequest(
-        symbol_or_symbols=symbols,
+        symbol_or_symbols=symbol,
         timeframe=timeframe,
         start=start,
         end=end
@@ -36,24 +36,22 @@ def get_stock_historical_data(stock_client, symbols, timeframe, start=None, end=
 
     bars = stock_client.get_stock_bars(request_params)
     bars_df = bars.df
-    return bars_df
+    return bars_df.loc[symbol]
 
 def main():
     account.get_account_info(config.KEYS)
-
-    stock_client = StockHistoricalDataClient(config.API_KEY,  config.SECRET_KEY)
     
+    stock_client = StockHistoricalDataClient(config.API_KEY,  config.SECRET_KEY)
     stock_index = 'qqq'
     with open(f'data/{stock_index}.csv') as f:
         qqq_holdings = f.readlines()
-    
     symbols = [holding.split(',')[2].strip() for holding in qqq_holdings[1:]]
     timeframe = TimeFrame.Day
     start = datetime.datetime(2022,1,1)
     for symbol in symbols:
+        # gets data frame of stock data
         df = get_stock_historical_data(stock_client, symbol, timeframe, start)
-
-        # technical indicators
+        # add technical indicators to data frame
         sma = btalib.sma(df, period = 30)
         rsi = btalib.rsi(df)
         macd = btalib.macd(df)
@@ -62,20 +60,16 @@ def main():
         df['macd'] = macd.df['macd']
         df['signal'] = macd.df['signal']
         df['histogram'] = macd.df['histogram']
-
+        # write data to file
         filename = f'data/daily/{symbol}.txt'
         with open(filename, 'w') as f:
-            f.write(df.loc[symbol].to_csv())
+            f.write(df.to_csv())
         
-    
-    df = pd.read_csv('data/daily/AAPL.txt', parse_dates=True, index_col='timestamp')
-    print(df)
-
-    # Getting financial data
-    # sf.set_data_dir('data/simfin/')
-    # sf.set_api_key(api_key=config.SIMFIN_KEY)
-    # df_income = sf.load_income(variant='annual', market='us')
-    # print(df_income.loc['AAPL'])
+    # Getting fundamental data
+    sf.set_data_dir('data/simfin/')
+    sf.set_api_key(api_key=config.SIMFIN_KEY)
+    df_income = sf.load_income(variant='annual', market='us')
+    print(df_income.loc['AAPL'])
         
 
 if __name__ == '__main__':
